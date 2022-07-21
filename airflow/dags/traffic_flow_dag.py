8c36b235a7dd29638be689266a14c536780a94f9
Nahom Bekele <nahom.fix@gmail.com>
2022-07-21 23:49:02 +0300

    feat: add dbt docker file

--- airflow/dags/traffic_flow_dag.py
+++ airflow/dags/traffic_flow_dag.py
@@ -1,11 +1,15 @@
+import os
 from datetime import datetime
 
 import pandas as pd
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+deployment = os.environ.get("DEPLOYMENT", "prod")
 
 
 def read_data():
@@ -15,20 +19,18 @@ def read_data():
         header=None,
         delimiter="\n",
     )
+
     return df.shape
 
 
 def insert_data():
-    pg_hook = PostgresHook(postgres_conn_id="postgres_localhost")
+    pg_hook = PostgresHook(postgres_conn_id=f"traffic_flow_{deployment}")
     conn = pg_hook.get_sqlalchemy_engine()
     df = pd.read_csv(
         "/opt/airflow/data/20181024_d1_0830_0900.csv",
-        skiprows=1,
-        header=None,
-        delimiter="\n",
+        sep="[,;:]",
+        index_col=False,
     )
-
-    df = df[0].str.split(";", expand=True)
 
     df.to_sql(
         "traffic_flow",
@@ -46,25 +48,35 @@ default_args = {
 }
 
 
-with DAG(
-    "simple_dag",
+dag = DAG(
+    "traffic_flow_dag",
     default_args=default_args,
-    schedule_interval="*/30 * * * *",
+    schedule_interval="@daily",
     catchup=False,
-) as dag:
+)
+envs = ["dev", "stg", "prod"]
+
+with dag:
+    start = DummyOperator(task_id="start")
+
+    dbt_test_op = BashOperator(
+        task_id="dbt_test",
+        bash_command="dbt run --profiles-dir /opt/airflow/dbt --project-dir /opt/airflow/dbt",
+    )
+
     read_data_op = PythonOperator(
         task_id="read_data", python_callable=read_data
     )
 
     create_table_op = PostgresOperator(
-        task_id="create_pg_table",
-        postgres_conn_id="postgres_localhost",
+        task_id=f"create_pg_table_{deployment}",
+        postgres_conn_id=f"traffic_flow_{deployment}",
         sql="""
             create table if not exists traffic_flow (
                 track_id integer,
                 type integer,
                 traveled_d integer,
-                avg_speed integer, 
+                avg_speed integer,
                 lat integer,
                 lon integer,
                 speed integer,
@@ -77,8 +89,7 @@ with DAG(
     )
 
     load_data_op = PythonOperator(
-        task_id="load_data", python_callable=insert_data
+        task_id=f"load_data_{deployment}", python_callable=insert_data
     )
 
-
-read_data_op >> create_table_op >> load_data_op
+    start >> dbt_test_op >> read_data_op >> create_table_op >> load_data_op
