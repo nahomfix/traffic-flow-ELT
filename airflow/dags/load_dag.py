b271ac0d00057f3cb2353aead2c5ffe9b7bb98b0
Nahom Bekele <nahom.fix@gmail.com>
2022-08-09 23:07:08 +0300

    chore: remove unused function

--- airflow/dags/load_dag.py
+++ airflow/dags/load_dag.py
@@ -14,49 +14,6 @@ def split_into_chunks(arr, n):
     return [arr[i : i + n] for i in range(0, len(arr), n)]
 
 
-def read_data():
-    import pandas as pd
-
-    df = pd.read_csv(
-        "/opt/airflow/data/20181024_d1_0830_0900.csv",
-        skiprows=1,
-        header=None,
-        delimiter="\n",
-    )
-
-    series = df[0].str.split(";")
-
-    pd_lines = []
-
-    for line in series:
-        old_line = [item.strip() for item in line]
-        info_index = 4
-        info = old_line[:info_index]
-        remaining = old_line[info_index:-1]
-        chunks = split_into_chunks(remaining, 6)
-        for chunk in chunks:
-            record = info + chunk
-            pd_lines.append(record)
-
-    new_df = pd.DataFrame(
-        pd_lines,
-        columns=[
-            "track_id",
-            "type",
-            "traveled_d",
-            "avg_speed",
-            "lat",
-            "lon",
-            "speed",
-            "lon_acc",
-            "lat_acc",
-            "time",
-        ],
-    )
-
-    return new_df.shape
-
-
 def insert_data():
     import pandas as pd
     from sqlalchemy.types import Integer, Numeric, String
@@ -100,34 +57,6 @@ def insert_data():
         ],
     )
 
-    # new_df[
-    #     [
-    #         "track_id",
-    #         "traveled_d",
-    #         "avg_speed",
-    #         "lat",
-    #         "lon",
-    #         "speed",
-    #         "lon_acc",
-    #         "lat_acc",
-    #         "time",
-    #     ]
-    # ] = new_df[
-    #     [
-    #         "track_id",
-    #         "traveled_d",
-    #         "avg_speed",
-    #         "lat",
-    #         "lon",
-    #         "speed",
-    #         "lon_acc",
-    #         "lat_acc",
-    #         "time",
-    #     ]
-    # ].apply(
-    #     pd.to_numeric
-    # )
-
     new_df.to_sql(
         "traffic_flow",
         con=conn,
@@ -147,16 +76,6 @@ def insert_data():
         },
     )
 
-    # tuples = [tuple(x) for x in new_df.to_numpy()]
-
-    # pg_hook.insert_rows(
-    #     "traffic_flow",
-    #     tuples,
-    #     replace=False,
-    #     target_fields=new_df.columns.tolist(),
-    #     commit_every=0,
-    # )
-
 
 default_args = {
     "owner": "airflow",
@@ -172,14 +91,9 @@ dag = DAG(
     schedule_interval="@daily",
     catchup=False,
 )
-envs = ["dev", "stg", "prod"]
 
 with dag:
     start = DummyOperator(task_id="start")
-
-    # read_data_op = PythonOperator(
-    #     task_id="read_data", python_callable=read_data
-    # )
 
     create_table_op = PostgresOperator(
         task_id=f"create_pg_table_{deployment}",
